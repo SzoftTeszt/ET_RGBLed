7062e3a6999201762ff2a416338c86af686684d2
Attila <szoftteszt2020@gmail.com>
2021-10-07 12:42:35 +0200

    LED teszt

--- RGBLED/RGBLED.ino
+++ RGBLED/RGBLED.ino
@@ -1,8 +1,21 @@
-int r=9;
+int r=11;
 int b=10;
-int g=11;
+int g=9;
 void setup() {
+  pinMode(r,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(g,OUTPUT);
 
+  digitalWrite(r,1);   delay(500);
+
+  digitalWrite(r,0);
+  digitalWrite(b,1);   delay(500);
+
+  digitalWrite(b,0);
+  digitalWrite(g,1);   delay(500);
+
+  digitalWrite(r,1);
+  digitalWrite(b,1);
 }
 
 void loop() {
